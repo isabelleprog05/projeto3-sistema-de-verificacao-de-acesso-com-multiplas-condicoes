273f03b2738ef186317b0af9037ca9eb07ab8992
Isabelle <isabellelopes8n@gmail.com>
2025-09-09 10:17:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let nome = parseInt(prompt("Digite seu nome: "))
+let idade = parseInt(prompt("Digite sua idade: "))
+
+if (idade >= 18) {
+    alert(`Olá, ${nome}! Você tem acesso completo. Boas-vindas à plataforma!`)
+} else if (nome === admin) {
+    alert("Bem-vindo, Admin! Acesso total liberado.")
+} else if (nome === moderador && idade >= 16) {
+    alert("Olá, moderador! Acesso liberado para a área de moderação.")
+} else if (idade >= 13 || idade <= 17) {
+    alert("Acesso negado. Você pode participar do nosso programa Jovem Aprendiz!")
+} else {
+    alert("Acesso negado. O conteúdo é exclusivo para maiores de 18 anos.")
+}
